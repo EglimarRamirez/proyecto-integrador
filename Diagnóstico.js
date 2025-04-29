6407726d68e40ef7076895a9aec4c0d4147e7aeb
Eglimar Ramirez <109255092+EglimarRamirez@users.noreply.github.com>
2025-04-29 00:02:46 -0300

    Add files via upload

---
+++ Diagnóstico.js
@@ -0,0 +1,54 @@
+// Actividad 1: Fundamentos de JavaScript
+
+// 1.1
+/*let nombre = "Ana"
+let edad = 22
+
+console.log(`"Hola, me llamo ${nombre} y tengo ${edad} años."`);
+
+//1.2
+
+const numeros = [3,7,12,5,2];
+
+// Mapeado
+let mapeado =numeros.map((numero)=>{return numero ** 2;});
+
+console.log(`Los numeros al cuadrado son ${mapeado}`);
+
+// Filtrado
+let filtrado = numeros.filter((numero)=>{return numero > 5;});
+
+console.log(`Los elementos mayores que cinco son ${filtrado}`);
+
+//1.3
+// Función flecha nro par o impar
+
+const clasnro = (nro)=>{
+    return nro % 2 === 0 ? "El nro es par":"El nro es impar";
+}
+
+console.log(clasnro(10));
+
+//Actividad 2: Desestructuración y objetos
+
+const persona = { nombre: "Lucía", edad: 28, profesion: "Diseñadora",};
+
+//2.1
+
+const {nombre, edad, profesion} = persona;
+
+console.log(`${nombre} tiene ${edad} años y trabaja como ${profesion}`);
+
+//2.2  
+
+persona.ciudad= "Rosario";
+
+console.log(persona);
+
+// Actividad 3: Funciones y callbacks
+
+Escribir una función que reciba un array y una función callback. 
+La función debe aplicar el callback a cada elemento del array y retornar el nuevo array.*/
+
+// Actividad 4: Primer componente en React
+
